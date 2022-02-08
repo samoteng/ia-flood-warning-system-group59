1ab3a7b7620ec2c4446750f57438b77546fb826b
Samuel Oteng-Ntim <sdo22@cam.ac.uk>
2022-02-08 20:37:49 +0000

    done

--- Task1F.py
+++ Task1F.py
@@ -1,10 +1,11 @@
 #Demonstration program
 #builds a list of all stations with inconsistent typical range data
 #print a list of station names, in alphabetical order
+def run():
+    from floodsystem.stationdata import build_station_list
+    stations = build_station_list()
 
-from floodsystem.stationdata import build_station_list
-stations = build_station_list()
-
-from floodsystem.station import inconsistent_typical_range_stations
-list = inconsistent_typical_range_stations(stations)
-print('Stations with inconsistant typical range data are: ', list)
+    from floodsystem.station import inconsistent_typical_range_stations
+    list = inconsistent_typical_range_stations(stations)
+    print('Stations with inconsistant typical range data are: ', list)
+run()
